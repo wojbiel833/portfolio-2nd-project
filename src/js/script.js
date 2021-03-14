d6fb9c85207efbb5eb55f6629aa23af48d37174c
Wojciech Bielecki <wojbiel833@gmail.com>
2021-03-14 20:21:02 +0100

    Add quit popup UI

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,33 @@
+// "use strict";
+import { select, classNames } from './settings.js';
 // $(document).ready(function () {
 //   $("#sidebarCollapse").on("click", function () {
 //     $("#sidebar").toggleClass("active");
 //   });
 // });
+
+// Elements
+
+const logOutBtn = document.querySelector(select.buttons.logOutBtn);
+console.log(logOutBtn);
+
+// Sections / Components
+
+const quitPopUp = document.querySelector(select.components.quitPopUp);
+console.log(quitPopUp);
+
+const quitPopUpOverlay = document.querySelector(
+  select.components.quitPopUpOverlay
+);
+console.log(quitPopUpOverlay);
+
+// Event handlers
+
+logOutBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+  quitPopUp.classList.remove('hidden');
+});
+logOutBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+  quitPopUpOverlay.classList.remove('hidden');
+});
---
+++ src/js/settings.js
@@ -0,0 +1,15 @@
+export const select = {
+  buttons: {
+    logOutBtn: '.log-out-btn',
+  },
+  components: {
+    quitPopUp: '.quit-popup',
+    quitPopUpOverlay: '.quit-popup-overlay',
+  },
+};
+
+export const classNames = {
+  popUps: {
+    show: '.hidden',
+  },
+};
