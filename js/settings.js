351d64f3c8114e5f760b2a203f00be41df12b482
Wojciech Bielecki <wojbiel833@gmail.com>
2021-04-10 13:34:11 +0200

    Initial dist subtree commit1

---
+++ js/settings.js
@@ -0,0 +1,48 @@
+export const select = {
+  sidebar: {
+    general: '#general',
+    details: '#details',
+    links: '#links',
+    banners: '#banners',
+    personalData: '#personal-data',
+    payouts: '#payouts',
+    postback: '#postback',
+  },
+  subpages: {
+    allSubpages: '[class^="section-"]',
+    mainPage: '.section-main-page',
+    detailsPage: '.section-details',
+    linksPage: '.section-links',
+    bannersPage: '.section-banners',
+    formPage: '.section-form',
+    payoutPage: '.section-payout-details',
+    postbackPage: '.section-new-postback',
+  },
+  buttons: {
+    logInBtn: '.log-in-link',
+    logOutBtn: '.log-out-btn',
+    closeWindowBtns: '.close-window-btn a',
+    chatButtons: '.sidebar-wrapper .friends li a',
+    sidebarShow: '#sidebar-show',
+  },
+  components: {
+    allModals: '.popup-overlay  > *',
+    navbarItems: '.from-navbar',
+    sidebar: '.st-container',
+    linksPopUp: '.add-links-popup',
+    bannersPopUp: '.add-banners-popup',
+    chatPopUp: '.popup-overlay .personal-chat-popup',
+    quitPopUp: '.quit-popup',
+    logInPopUp: '.login-popup ',
+    popUpOverlay: '.popup-overlay',
+  },
+};
+
+export const classNames = {
+  popUps: {
+    hidden: 'hidden',
+  },
+  sidebar: {
+    active: 'active',
+  },
+};
